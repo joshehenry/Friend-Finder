0b5c899ab166521522d6f178d3fa2f2113e73c0e
Aurom Thebes <joshehenry@yahoo.com>
2018-11-11 17:03:43 -0600

    Created server, additional work with routing and HTML

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friends = {
+
+name: Goku,
+photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjWuOGiuM3eAhUlh-AKHQZqD9QQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D8A5rA-XQMwg&psig=AOvVaw1LXfNFUg3myXybbim-dogf&ust=1542063479672530",
+scores: [3, 1, 5, 5, 1, 4, 2, 2, 4, 3 ] ,
+
+name: Vegeta, 
+photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi3ttaXuM3eAhVnhOAKHWoXCioQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F648729521298229939%2F&psig=AOvVaw2qCYXLire1xtRccXgOQLlA&ust=1542063451775964',
+scores: [1, 5, 2, 3, 5, 4, 2, 1, 4, 5] , 
+
+name: Gohan,
+photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj8x83xt83eAhWJUt8KHfmDDOsQjRx6BAgBEAU&url=https%3A%2F%2Fentertainment.desktopnexus.com%2Fwallpaper%2F2122218%2F&psig=AOvVaw3cTl_9VfI-mrY_ZXI0LYmQ&ust=1542063373259088',
+scores: [3, 3, 1, 4, 5, 5, 3, 2, 1, 3], 
+
+name: Jiren,
+photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjy4o3Zt83eAhVDiOAKHbKdAXcQjRx6BAgBEAU&url=https%3A%2F%2Faminoapps.com%2Fc%2Fdragonballz%2Fpage%2Fitem%2Fjiren-the-gray%2FJ8LT_MIleLoPaVpowJ80vR0oEYmed73&psig=AOvVaw35xMZg8KDAWpZdpVOOMmnn&ust=1542063320858842',
+scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 5] , 
+
+name: Uub,
+photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiv4aG0uM3eAhXjRt8KHef7C44QjRx6BAgBEAU&url=http%3A%2F%2Fdragonball.wikia.com%2Fwiki%2FUub&psig=AOvVaw3bAFFyntFe4Hdx_B3wSBxq&ust=1542063503464938',
+scores: [3, 2, 3, 4, 5, 3, 2, 1, 4, 5]
+
+}
+
+module.exports = friends; 
---
+++ app/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+	
+	console.log("Server listening on: http://localhost:" + PORT);
+});
